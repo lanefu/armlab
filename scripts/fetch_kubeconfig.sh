cefe6e0c96dbf17cd9379aab499715c758e4e075
Lane Jennison <lane@lane-fu.com>
2024-06-15 07:43:28 -0400

    script: fetch_kubeconfig.sh as lazy temporary solution

---
+++ scripts/fetch_kubeconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## temporary script.. really should be using kube-vip and ansible and setting a hostname etc
+
+HOST=${1}
+
+if [[ -z ${HOST} ]]; then
+	echo "provide host target as paramter"
+	exit 1
+fi
+
+ssh ${HOST} "sudo cat /etc/rancher/k3s/k3s.yaml" | sed -e "s/https:\/\/127.0.0.1/https:\/\/${HOST}/" > kubeconfig
