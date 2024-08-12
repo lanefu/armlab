9806da6e1e26725cff69aedba52d1a0ebccf9008
Lane Jennison <lane@lane-fu.com>
2024-08-11 22:54:40 -0400

    add flux

--- scripts/big_bang.sh
+++ scripts/big_bang.sh
@@ -7,7 +7,7 @@ done
 
 ansible-playbook playbooks/provision_k8s_nodes.yml -e @vars/local.yml -u root
 #sleep on mac is stupid
-SLEEP_MIN=2
+SLEEP_MIN=3
 echo "## wait ${SLEEP_MIN} min for cloud-init dance"
 SLEEP_SEC=$(echo "${SLEEP_MIN} * 60" | bc)
 sleep ${SLEEP_SEC}
@@ -22,4 +22,6 @@ scripts/install_kube-vip_arp.sh
 kubectl -n kube-system get pods
 kubectl apply -f https://raw.githubusercontent.com/inlets/inlets-operator/master/contrib/nginx-sample-deployment.yaml -n default
 kubectl expose deployment nginx-1 --port=80 --type=LoadBalancer -n default --load-balancer-ip=172.17.20.41
-kubectl get services
+./scripts/install_traefik_with_kube-vip.sh
+./scripts/bootstrap_flux.sh
+kubectl get services --all-namespaces
---
+++ scripts/bootstrap_flux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source armlab_flux.conf
+
+flux bootstrap git \
+  --url=${FLUX_REPO_URL} \
+  --branch="${FLUX_BRANCH}" \
+  --private-key-file="${FLUX_PRIVATE_KEY_FILE}" \
+  --path="${FLUX_PATH}" \
+  --verbose
