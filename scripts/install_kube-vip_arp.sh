ee839ffa7bc0ce80f84c44d341e03cc8662c03e4
Lane Jennison <lane@lane-fu.com>
2024-10-27 07:44:10 -0400

    kube-vip: tweak script not to generate if file exists

--- scripts/install_kube-vip_arp.sh
+++ scripts/install_kube-vip_arp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./scripts/generate_kube-vip_manifest_arp.sh > files/manifests/kube-vip_daemonset.yaml
+# generation tool requires docker so lets just skip if it exists to same time
+if [ ! -f files/manifests/kube-vip_daemonset.yaml ]; then
+  ./scripts/generate_kube-vip_manifest_arp.sh >files/manifests/kube-vip_daemonset.yaml
+fi
 
 kubectl apply -f https://kube-vip.io/manifests/rbac.yaml
 kubectl apply -f files/manifests/kube-vip_daemonset.yaml
